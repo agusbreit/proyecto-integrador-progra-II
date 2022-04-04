05e90b35d36da0a4fbb5223ac841ea8ff169183b
agusbreit <abreitfeld@udesa.edu.ar>
2022-04-04 16:09:18 -0300

    prueba nodemon

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const usersController = require ('../controllers/usersController')
 
-router.get('/', usersController.index/*.FALTA EL METODO QUE QUIERO LLAMAR*/);
+router.get('/', usersController.index);
 
 module.exports = router;  
