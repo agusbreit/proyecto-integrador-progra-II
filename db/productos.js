c0718757ad132853acca9b314ed3e0b96232aa4f
Ignaciadoldan <idoldandominguez@udesa.edu.ar>
2022-04-07 17:57:10 -0300

    creando info de data base

---
+++ db/productos.js
@@ -0,0 +1,53 @@
+var productos= [
+     {  nombre: 'Lenguetazos',
+        descripcion: 'Caramelos lenguetazos en tres sabores: pinta lenguas, tutifruti y fiesta.',
+        image: '',
+     },
+				
+     { nombre: 'Chicle Beldent',
+     descripcion: 'Chicles Beldent sabor menta. 8 unidades, frecura garantizada.',
+     image: '',
+     },
+     
+     { nombre: 'Sugus confitados',
+     descripcion: 'Sugus confitados sabor frutilla, moras y limon',
+     image: '',
+     },
+
+     { nombre: 'Alfajor Milka Oreo',
+     descripcion: 'Alfajor torta sabor Oreo y con el mejor chocolate Milka, delicioso.',
+     image: '',
+     },
+    
+     { nombre: 'Chocolate Arcor',
+     descripcion: 'Chocolate con leche y mani, apto para celiacos',
+     image: '',
+     },
+
+     { nombre: 'Chocolate Block',
+     descripcion: 'Chocolate con leche y mani marca Cofler. 38 g la unidad, irresistible.',
+     image: '',
+     },
+
+     { nombre: 'Cerebritos Mogul',
+     descripcion: 'Gomitas rellenas en forma de cerebrito marcra Mogul. Sabores limon, fresa, banana y manzana.',
+     image: '',
+     },
+
+     { nombre: 'Caramelos rellenos',
+     descripcion: 'Caramelos rellenos frutales Arcor. Sabor cereza, banana, manzana, durazno y anana.',
+     image: '',
+     },
+
+     { nombre: 'Rocklets',
+     descripcion: 'Paquetes de rocklets de todos los colores. Rellenos de mani o chocolate',
+     image: '',
+     },
+
+     { nombre: 'Chupetin',
+     descripcion: 'Chupetines sabor frutilla y limon',
+     image: '',
+     },
+]
+
+module.exports= productos;
