b59ab809a6443850f8e564227707bc9f2ff19f73
Mati2108 <matiascalo2@gmail.com>
2022-04-07 16:51:27 -0300

    headerlogueado

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const indexController = require ('../controllers/indexController');
 
+
 router.get('/', indexController.index);
 
 router.get('/search-results', indexController.searchResults);
