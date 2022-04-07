e27d396fba92e206877bb57b31b5c8e99695c7de
agusbreit <abreitfeld@udesa.edu.ar>
2022-04-07 20:48:58 -0300

    listo de moficiaciones

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,27 +1,16 @@
 var usuario = require('../db/usuario');
-<<<<<<< HEAD
 var productos= require('../db/productos');
-
-var productController = {
-    index: function (req, res) {
-        res.render('product', {'usuario': usuario}, /*{'productos': productos }*/) ;
-=======
 var comentarios = require('../db/comentarios');
 
 var productController = {
     index: function (req, res) {
-<<<<<<< HEAD
-        res.render('product', {'usuario': usuario})
+        res.render('product', {'usuario': usuario}, /*{'productos': productos }*/)
     },
     productAdd: function (req, res) {
-        res.render('product-add', {'usuario': usuario}, {'comentarios': comentarios} );
-=======
-        res.render('product', {'usuario': usuario}, {'productos': productos }) ;
->>>>>>> 65641be6da9abfdcc2423275e284432baa4996b5
+        res.render('product-add', {'usuario': usuario}, /*{'comentarios': comentarios}*/ );
     },
     productAdd: function (req, res) {
         res.render('product-add', {'usuario': usuario}) ;
->>>>>>> 361fec627997bc79faf630fd1f6685bdf7c3f6fa
     }
 };
 
