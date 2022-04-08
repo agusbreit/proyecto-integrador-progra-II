3369dc41396f561b0e94e0dd2db64f438c81cfa2
Mati2108 <matiascalo2@gmail.com>
2022-04-08 09:25:41 -0300

    Boton funcionando que va a search results

--- db/productos.js
+++ db/productos.js
@@ -1,89 +1,61 @@
-var productos= [
-<<<<<<< HEAD
-     {  nombre: 'Lenguetazos',
-        descripcion: 'Caramelos lenguetazos en tres sabores: pinta lenguas, tutifruti y fiesta.',
-        image: '/images/products/lenguetazo.png',
-     },
-				
-     { nombre: 'Chicle Beldent',
-     descripcion: 'Chicles Beldent sabor menta. 8 unidades, frecura garantizada.',
-     image: '/images/products/beldent menta.webp',
-     },
-     
-     { nombre: 'Sugus confitados',
-     descripcion: 'Sugus confitados sabor frutilla, moras y limon',
-     image: '/images/products/sugus-confitados-1.jpeg',
-     },
-
-     { nombre: 'Alfajor Milka Oreo',
-     descripcion: 'Alfajor torta sabor Oreo y con el mejor chocolate Milka, delicioso.',
-     image: '/images/products/alfajor milka.webp',
-     },
-    
-     { nombre: 'Chocolate Arcor',
-     descripcion: 'Chocolate con leche y mani, apto para celiacos',
-     image: '/images/products/chocolate arcor.jpeg',
-     },
-
-     { nombre: 'Chocolate Block',
-     descripcion: 'Chocolate con leche y mani marca Cofler. 38 g la unidad, irresistible.',
-     image: '/images/products/block chocolate.jpeg',
-     },
-
-     { nombre: 'Cerebritos Mogul',
-     descripcion: 'Gomitas rellenas en forma de cerebrito marcra Mogul. Sabores limon, fresa, banana y manzana.',
-     image: '/images/products/mogul.jpeg',
-     },
-=======
-   {  nombre: 'Lenguetazos',
+var productos = [{
+      nombre: 'Lenguetazos',
       descripcion: 'Caramelos lenguetazos en tres sabores: pinta lenguas, tutifruti y fiesta.',
       image: '/images/products/lenguetazo.png',
    },
-          
-   { nombre: 'Chicle Beldent',
-   descripcion: 'Chicles Beldent sabor menta. 8 unidades, frecura garantizada.',
-   image: '/images/products/beldent menta.webp',
-   },
-   
-   { nombre: 'Sugus confitados',
-   descripcion: 'Sugus confitados sabor frutilla, moras y limon',
-   image: '/images/products/sugus-confitados-1.jpeg',
+
+   {
+      nombre: 'Chicle Beldent',
+      descripcion: 'Chicles Beldent sabor menta. 8 unidades, frecura garantizada.',
+      image: '/images/products/beldent menta.webp',
    },
 
-   { nombre: 'Alfajor Milka Oreo',
-   descripcion: 'Alfajor torta sabor Oreo y con el mejor chocolate Milka, delicioso.',
-   image: '/images/products/alfajor milka.webp',
-   },
-  
-   { nombre: 'Chocolate Arcor',
-   descripcion: 'Chocolate con leche y mani, apto para celiacos',
-   image: '/images/products/chocolate arcor.jpeg',
+   {
+      nombre: 'Sugus confitados',
+      descripcion: 'Sugus confitados sabor frutilla, moras y limon',
+      image: '/images/products/sugus-confitados-1.jpeg',
    },
 
-   { nombre: 'Chocolate Block',
-   descripcion: 'Chocolate con leche y mani marca Cofler. 38 g la unidad, irresistible.',
-   image: '/images/products/block chocolate.jpeg',
+   {
+      nombre: 'Alfajor Milka Oreo',
+      descripcion: 'Alfajor torta sabor Oreo y con el mejor chocolate Milka, delicioso.',
+      image: '/images/products/alfajor milka.webp',
    },
 
-   { nombre: 'Cerebritos Mogul',
-   descripcion: 'Gomitas rellenas en forma de cerebrito marcra Mogul. Sabores limon, fresa, banana y manzana.',
-   image: '/images/products/mogul.jpeg',
-   },
->>>>>>> feaaf6bb0062baac053cb42d3483a8156e7df0eb
-
-   { nombre: 'Caramelos rellenos',
-   descripcion: 'Caramelos rellenos frutales Arcor. Sabor cereza, banana, manzana, durazno y anana.',
-   image: '/images/products/caramelo.png',
+   {
+      nombre: 'Chocolate Arcor',
+      descripcion: 'Chocolate con leche y mani, apto para celiacos',
+      image: '/images/products/chocolate arcor.jpeg',
    },
 
-   { nombre: 'Rocklets',
-   descripcion: 'Paquetes de rocklets de todos los colores. Rellenos de mani o chocolate',
-   image: '/images/products/rockelts.webp',
+   {
+      nombre: 'Chocolate Block',
+      descripcion: 'Chocolate con leche y mani marca Cofler. 38 g la unidad, irresistible.',
+      image: '/images/products/block chocolate.jpeg',
    },
 
-   { nombre: 'Chupetin',
-   descripcion: 'Chupetines sabor frutilla y limon',
-   image: '/images/products/chupetin.webp',
+   {
+      nombre: 'Cerebritos Mogul',
+      descripcion: 'Gomitas rellenas en forma de cerebrito marcra Mogul. Sabores limon, fresa, banana y manzana.',
+      image: '/images/products/mogul.jpeg',
+   },
+
+   {
+      nombre: 'Caramelos rellenos',
+      descripcion: 'Caramelos rellenos frutales Arcor. Sabor cereza, banana, manzana, durazno y anana.',
+      image: '/images/products/caramelo.png',
+   },
+
+   {
+      nombre: 'Rocklets',
+      descripcion: 'Paquetes de rocklets de todos los colores. Rellenos de mani o chocolate',
+      image: '/images/products/rockelts.webp',
+   },
+
+   {
+      nombre: 'Chupetin',
+      descripcion: 'Chupetines sabor frutilla y limon',
+      image: '/images/products/chupetin.webp',
    },
 ]
-module.exports= productos;
+module.exports = productos;
