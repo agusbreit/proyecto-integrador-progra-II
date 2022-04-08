91689513f4fafc836313f0d3e16f64eaec009d2e
Mati2108 <matiascalo2@gmail.com>
2022-04-08 10:02:31 -0300

    mini arreglos y comentarios

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 const indexController = require ('../controllers/indexController');
 
-
 router.get('/', indexController.index);
 
-router.get('/search-results', indexController.searchResults);
+router.get('/search-results/:idProducto?', indexController.searchResults);
 
 module.exports = router;
