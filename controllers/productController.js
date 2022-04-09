6434af7693b18c1ceb619c36e03dd83bbabfa760
agusbreit <abreitfeld@udesa.edu.ar>
2022-04-09 20:11:44 -0300

    detalle de producto funciona, cambia dependiendo del :id

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,7 +4,9 @@ var comentarios = require('../db/comentarios');
 
 var productController = {
     index: function (req, res) {
-        res.render('product', {'usuario': usuario, 'productos': productos, 'comentarios': comentarios }) },
+        res.render('product', {usuario: usuario, productos: productos, comentarios: comentarios, idProduct: req.params.id });
+        
+    },
 
     productAdd: function (req, res) {
         res.render('product-add', {'usuario': usuario, 'comentarios': comentarios });
--- routes/product.js
+++ routes/product.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 const productController = require ('../controllers/productController')
 
-router.get('/:id?', productController.index);
-
-
-
 router.get('/product-add', productController.productAdd)
+
+router.get('/:id?', productController.index);
 
 module.exports = router;
