c3d97f8e59984d41855d403cffee649d6ec5ecbb
System Administrator <root@MacBook-Pro-de-Ignacia.local>
2022-05-30 19:09:48 -0300

    creando modelos

---
+++ database/models/Producto.js
@@ -0,0 +1,66 @@
+module.exports = function (sequelize, dataTypes){
+
+    let alias = 'Producto';
+
+    let cols = {
+        id:{
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER,
+        },
+
+        nombre:{
+            type:dataTypes.STRING
+        },
+
+        imagen:{
+            type:dataTypes.STRING
+        },
+
+        descripcion:{
+            type:dataTypes.STRING
+        },
+
+        usuarioId:{
+            type:dataTypes.INTEGER
+        },
+
+        createdAt: {
+            type: dataTypes.DATE,
+            allowNull: true
+        },
+        updatedAt: {
+            type: dataTypes.DATE,
+            allowNull: true
+        },
+        delatedAt: {
+            type: dataTypes.DATE,
+            allowNull: true
+        },
+
+    }
+
+    //Configuraciones adicionales
+    let config = {
+        tableName: 'productos', //Nombre de la tabla en la base de datos.
+        timestamps: true, //Si la tabla no tiene los campos createdAt y updatedAt
+    }
+
+    const Producto = sequelize.define(alias, cols, config);
+
+    //Relaciones entre tablas.
+    Producto.associate = function(models){
+
+        Producto.belongsTo(models.Usuario,{
+            as:'usuario',
+            foreignKey: 'UsuarioId'
+        })
+        Producto.hasMany(models.Comentario, {
+            as:'comentario',
+            foreignKey:'ProductoId'
+        })
+  
+    }
+
+    return Producto;
+}
